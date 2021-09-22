7470cc330b204d279303e80238d5547df8024688
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-22 20:25:45 -0300

    Declarando classe Televisão

--- src/Exercicio4/Televisao.java
+++ src/Exercicio4/Televisao.java
@@ -2,4 +2,43 @@ package Exercicio4;
 
 public class Televisao {
 
+    private int volume;
+    private int canal;
+    private ControleRemoto controleRemoto;
+
+    //construtor
+    public Televisao(int volume, int canal){
+
+        this.volume = volume;
+        this.canal = canal;
+
+    }
+
+    //getters e setters
+
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public ControleRemoto getControleRemoto() {
+        return controleRemoto;
+    }
+
+    public void setControleRemoto(ControleRemoto controleRemoto) {
+        this.controleRemoto = controleRemoto;
+    }
+
 }
