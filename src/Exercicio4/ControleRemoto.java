5f371a170e1d4e200ec45949d565d05e9d5af284
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-22 20:18:18 -0300

    Declarando classe Controle Remoto

--- src/Exercicio4/ControleRemoto.java
+++ src/Exercicio4/ControleRemoto.java
@@ -2,5 +2,48 @@ package Exercicio4;
 
 public class ControleRemoto {
 
+    private int aumentarVolume;
+    private int diminuirVolume;
+    private int passarCanal;
+    private int voltarCanal;
+
+    //construtor
+    public ControleRemoto(){
+
+    }
+
+    //getters e setters
+
+    public int getAumentarVolume() {
+        return aumentarVolume;
+    }
+
+    public void setAumentarVolume(int aumentarVolume) {
+        this.aumentarVolume++;
+    }
+
+    public int getDiminuirVolume() {
+        return diminuirVolume;
+    }
+
+    public void setDiminuirVolume(int diminuirVolume) {
+        this.diminuirVolume--;
+    }
+
+    public int getPassarCanal() {
+        return passarCanal;
+    }
+
+    public void setPassarCanal(int passarCanal) {
+        this.passarCanal++;
+    }
+
+    public int getVoltarCanal() {
+        return voltarCanal;
+    }
+
+    public void setVoltarCanal(int voltarCanal) {
+        this.voltarCanal--;
+    }
 
 }
