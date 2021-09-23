ad44dd1e56adbe21be6394360b40f9de71fca3cd
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-23 08:47:22 -0300

    Adicionando condições para alterações dos Dados

--- src/Exercicio4/ControleRemoto.java
+++ src/Exercicio4/ControleRemoto.java
@@ -9,23 +9,49 @@ public class ControleRemoto{
     }
 
     public void aumentarvolume(){
-        int aumentar = tv.getVolume() + 1 ;
-        tv.setVolume(aumentar);
+
+        if(tv.getVolume() < 100){
+            int aumentar = tv.getVolume() + 1 ;
+            tv.setVolume(aumentar);
+        }else {
+            System.out.println("Volume maximo atingido: " + tv.getVolume());
+            System.out.println("Atenção: Volume muito alto pode prejudicar sua audição!");
+        }
+
     }
 
     public void diminuirVolume(){
-        int diminuir = tv.getVolume() - 1;
-        tv.setVolume(diminuir);
+
+        if (tv.getVolume() > 0){
+            int diminuir = tv.getVolume() - 1;
+            tv.setVolume(diminuir);
+        }else{
+            System.out.println("A TV já está no mudo");
+        }
+
     }
 
+
     public void passarCanal(){
-        int passar = tv.getCanal() + 1;
-        tv.setCanal(passar);
+
+        if(tv.getCanal() > 0){
+            int passar = tv.getCanal() + 1;
+            tv.setCanal(passar);
+        }else {
+            System.out.println("Erro!");
+        }
+
     }
 
     public void voltarCanal(){
-        int voltar = tv.getCanal() - 1;
-        tv.setCanal(voltar);
+
+        if (tv.getCanal() > 0){
+            int voltar = tv.getCanal() - 1;
+            tv.setCanal(voltar);
+        }else {
+            System.out.println("Erro!");
+        }
+
     }
 
     public void selecionarCanal(int canal){
